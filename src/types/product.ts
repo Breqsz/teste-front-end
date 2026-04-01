a0418672e438d1c63624cc392d318b7f5e0609ea
Breq1337 <guirochabianchini@gmail.com>
2026-04-01 20:35:33 -0300

    Product types

---
+++ src/types/product.ts
@@ -0,0 +1,7 @@
+// Espelha um item do array `products` no JSON da API (nomes dos campos iguais ao backend)
+export interface Product {
+  productName: string
+  descriptionShort: string
+  photo: string
+  price: number
+}
